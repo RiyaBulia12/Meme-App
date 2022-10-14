a4509641f2fda8b35ea02c66c2e34e6ab42173f2
Apple <riyabulia12@gmail.com>
2022-10-15 03:54:51 +0530

    Create Meme generator app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Navigation } from "./components/Navigation";
+import { MemeForm } from './components/MemeForm';
+
+function App() {
+  return (
+     <div>
+        <Navigation />
+        <MemeForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+export const Navigation = () => {
+   return(
+      <>
+         <header>
+            <nav className="py-6 px-8 shadow-md bg-zinc-900">
+               <h1 className="text-3xl font-bold text-center">Meme Generator</h1>
+            </nav>
+         </header>
+      </>
+   )
+}
